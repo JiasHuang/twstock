139b965408ccf24030b43868c55973be417e01a4
jiashuang <jias.tw@gmail.com>
2020-11-22 09:59:19 +0800

    add tse_code_list.txt

--- code_list.py
+++ code_list.py
@@ -1,18 +1,27 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os
 import re
-import sys
 
 import xurl
 
-def main():
+def gen_tse():
+    url = 'https://isin.twse.com.tw/isin/C_public.jsp?strMode=2'
+    txt = xurl.load(url)
+    m = re.findall(r'<tr><td bgcolor=#FAFAD2>(\w+)', txt)
+    xurl.saveLocal('tse_code_list.txt', '\n'.join(m))
+    return
+
+def gen_otc():
     url = 'https://isin.twse.com.tw/isin/C_public.jsp?strMode=4'
     txt = xurl.load(url)
     m = re.findall(r'<tr><td bgcolor=#FAFAD2>(\w+)', txt)
     xurl.saveLocal('otc_code_list.txt', '\n'.join(m))
     return
 
+def main():
+    gen_tse()
+    gen_otc()
+ 
 if __name__ == '__main__':
     main()

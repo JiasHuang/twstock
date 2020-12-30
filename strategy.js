3be498aed101764072cb91605e99c228f45c7739
jiashuang <jias.tw@gmail.com>
2020-12-30 10:06:22 +0800

    refine strategy

--- strategy.js
+++ strategy.js
@@ -1,5 +1,7 @@
 
+var strategy = null;
 var account = null;
+var info = null;
 var all_total_cost = 0;
 
 String.format = function() {
@@ -70,32 +72,51 @@ function getStat(s) {
   return st;
 }
 
-function parsestrategyJSON(obj) {
+function updateResult() {
   var text = '';
+  var td_na = '<td><span class="grey">-</span></td>';
 
-  console.log(obj);
+  if (!strategy || !account || !info) {
+    return;
+  }
+
+  if (strategy.stocks.length != info.stocks.length) {
+    console.log('strategy/info mismatch');
+    return;
+  }
 
   text += '<table id="stocks">';
-  text += '<tr><th rowspan=2>代碼</th><th rowspan=2>名稱</th><th rowspan=2>參考價</th><th rowspan=2>預估張數</th>';
-  text += '<th rowspan=2 colspan=2>分批1</th><th rowspan=2 colspan=2>分批2</th><th rowspan=2 colspan=2>分批3</th><th colspan=4>統計</th></tr>';
-  text += '<th>張數</th><th>剩餘</th><th>均價</th><th>成本</th>';
-  text +='</tr>';
+  text += '<tr><th>代碼</th><th>名稱</th><th>參考價</th><th>預估張數</th>';
+  text += '<th>市價</th><th colspan=2>批1</th><th colspan=2>批2</th><th colspan=2>批3</th><th>張數</th><th>剩餘</th><th>均價</th><th>成本</th>';
+  text += '</tr>';
 
-  for (var i=0; i<obj.stocks.length; i++) {
-    let s = obj.stocks[i];
+  for (var i=0; i<strategy.stocks.length; i++) {
+    let s = strategy.stocks[i];
     let st = getStat(s);
+    let z = info.stocks[i].z;
+    let cls = Array(3).fill('grey');
+    let z_cls = 'grey';
+
+    for (var j=2; j>=0; j--) {
+      if (z <= st.ref[j]) {
+        cls[j] = z_cls = 'bg_yellow';
+        break;
+      }
+    }
+
     text += '<tr>';
     text += String.format('<td class="edit" contenteditable=true>{0}</td>', s.code);
     text += String.format('<td class="edit" contenteditable=true>{0}</td>', s.name);
     text += String.format('<td class="edit" contenteditable=true>{0}</td>', s.ref_pz);
     text += String.format('<td class="edit" contenteditable=true>{0}</td>', s.ref_qty);
+    text += String.format('<td><span class="{0}">{1}</span></td>', z_cls, z.toFixed(2));
     for (var j=0; j<3; j++) {
-      text += String.format('<td><={0}</td>', st.ref[j].toFixed(2));
+      text += String.format('<td><span class="{0}"><={1}</span></td>', cls[j], st.ref[j].toFixed(2));
       if (st.qty[j]) {
         let avg = st.cost[j] / 1000 / st.qty[j];
         text += String.format('<td>{0} x {1}</td>', avg.toFixed(2), st.qty[j]);
       } else {
-        text += String.format('<td>-</td>');
+        text += td_na;
       }
     }
     if (st.total_cost) {
@@ -105,7 +126,7 @@ function parsestrategyJSON(obj) {
       text += String.format('<td>{0}</td>', total_avg.toFixed(2));
       text += String.format('<td>{0}</td>', st.total_cost.toLocaleString());
     } else {
-      text += '<td>-</td>'.repeat(4);
+      text += td_na.repeat(4);
     }
     text += '</tr>';
   }
@@ -113,7 +134,7 @@ function parsestrategyJSON(obj) {
   for (var i=0; i<3; i++) {
     text += '<tr>';
     text += '<td contenteditable=true></td>'.repeat(4);
-    text += '<td>-</td>'.repeat(10);
+    text += td_na.repeat(11);
     text += '</tr>';
   }
 
@@ -129,12 +150,40 @@ function onTimeout() {
   console.log('timeout');
 }
 
-function loadstrategyJSON() {
+function parseStocksJSON(obj) {
+  console.log(obj);
+  info = obj;
+  updateResult();
+}
+
+function loadStocksJSON(obj) {
+  var codes = [];
+
+  for (var i=0; i<strategy.stocks.length; i++) {
+    codes.push(strategy.stocks[i].code);
+  }
+
+  $.ajax({
+    url: String.format('view.py?c={0}', codes.join(',')),
+    dataType: 'json',
+    error: onTimeout,
+    success: parseStocksJSON,
+    timeout: 10000
+  });
+}
+
+function parseStrategyJSON(obj) {
+  console.log(obj);
+  strategy = obj;
+  loadStocksJSON(obj);
+}
+ 
+function loadStrategyJSON() {
   $.ajax({
     url: 'load.py?j=strategy.json',
     dataType: 'json',
     error: onTimeout,
-    success: parsestrategyJSON,
+    success: parseStrategyJSON,
     timeout: 2000
   });
 }
@@ -142,7 +191,7 @@ function loadstrategyJSON() {
 function parseAccountJSON(obj) {
   console.log(obj);
   account = obj;
-  loadstrategyJSON();
+  updateResult();
 }
 
 function loadAccountJSON() {
@@ -162,7 +211,7 @@ function onSuccess() {
 function onSave() {
   var table = document.getElementById("stocks");
   var jsons = [];
-  for (var i = 2, row; row = table.rows[i]; i++) {
+  for (var i = 1, row; row = table.rows[i]; i++) {
     let code = row.cells[0].textContent;
     let name = row.cells[1].textContent;
     let ref_pz = row.cells[2].textContent;
@@ -188,5 +237,6 @@ function onSave() {
 
 function onDocumentReady() {
   loadAccountJSON();
+  loadStrategyJSON();
 }
 
